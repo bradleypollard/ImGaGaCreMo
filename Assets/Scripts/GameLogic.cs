d8be5e0e1e30fb745d89726ef46282450108d8e5
bradleypollard <bp912@ic.ac.uk>
2016-01-20 17:08:59 +0000

    Initial commit

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+    public Galaxy g;
+    public GameObject galaxy;
+
+	// Use this for initialization
+	void Start () {
+        g = new Galaxy();
+        Debug.Log(g.DebugGalaxy());
+
+        SolarSystem[] systems = g.GetSystems();
+        foreach (SolarSystem s in systems)
+        {
+            Vector3 pos = new Vector3(s.x, s.y, 0f);
+
+            Instantiate(galaxy, pos, Quaternion.identity);
+            foreach (SolarSystem neighbour in s.neighbours)
+            {
+                Debug.DrawLine(pos, new Vector3(neighbour.x, neighbour.y, 0f), Color.green, 100000000000f);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
---
+++ Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SolarSystem {
+
+    public int x;
+    public int y;
+    public List<SolarSystem> neighbours;
+
+    private int m_numPlanets;
+
+    public SolarSystem( int _numPlanets )
+    {
+        m_numPlanets = _numPlanets;
+        neighbours = new List<SolarSystem>();
+    }
+
+}
