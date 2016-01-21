727efee61d857eebf3456553f4b040067eae5408
bradleypollard <bp912@ic.ac.uk>
2016-01-21 22:28:38 +0000

    Mouse movement

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -13,6 +13,8 @@ public class GameLogic : MonoBehaviour
 
   public Material[] starMaterials = new Material[SolarSystem.s_starTypes.Length];
 
+  private bool mouseDown = false;
+
   // Use this for initialization
   void Start()
   {
@@ -44,6 +46,23 @@ public class GameLogic : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    if (Input.GetMouseButtonDown(2))
+    {
+      mouseDown = true;
+      Cursor.visible = false;
+      Cursor.lockState = CursorLockMode.Locked;
 
+    }
+    else if (Input.GetMouseButtonUp(2))
+    {
+      mouseDown = false;
+      Cursor.visible = true;
+      Cursor.lockState = CursorLockMode.None;
+    }
+    
+    if (mouseDown)
+    {
+      Camera.main.transform.position -= new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f) * 0.1f;
+    }
   }
 }
